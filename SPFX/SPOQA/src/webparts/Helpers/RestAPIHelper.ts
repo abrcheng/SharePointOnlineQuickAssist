5ae8844c6d277b97ea8aaad6ed559a3749605240
Abraham Cheng <abrcheng@microsoft.com>
2021-10-06 15:55:34 +0800

    Added all fix logic for the SearchDocument
    Added all fix logic for the SearchDocument

--- SPFX/SPOQA/src/webparts/Helpers/RestAPIHelper.ts
+++ SPFX/SPOQA/src/webparts/Helpers/RestAPIHelper.ts
@@ -231,13 +231,13 @@ export default class RestAPIHelper
               "Accept": "application/json;odata=verbose",            
               "Content-Type": "application/json;odata=verbose",            
               "IF-MATCH": "*",            
-              "X-HTTP-Method": "MERGE"            
+              "X-HTTP-Method": "MERGE",
+              "odata-version": "3.0"               
             },  
             body: JSON.stringify(listData)  
         };  
-      var res = await spHttpClient.post(apiUrl, SPHttpClient.configurations.v1, spOpts); 
-      var resJson = await res.json();
-      return resJson;
+      var res = await spHttpClient.post(apiUrl, SPHttpClient.configurations.v1, spOpts);      
+      return res;
     }
   
     public static async FixWebNoCrawl(spHttpClient:SPHttpClient, siteAbsoluteUrl:string)
@@ -256,23 +256,147 @@ export default class RestAPIHelper
               "Accept": "application/json;odata=verbose",            
               "Content-Type": "application/json;odata=verbose",            
               "IF-MATCH": "*",            
-              "X-HTTP-Method": "MERGE"            
+              "X-HTTP-Method": "MERGE",
+              "odata-version": "3.0"            
             },  
             body: JSON.stringify(listData)  
         };  
-      var res = await spHttpClient.post(apiUrl, SPHttpClient.configurations.v1, spOpts); 
-      var resJson = await res.json();
-      return resJson;
+      var res = await spHttpClient.post(apiUrl, SPHttpClient.configurations.v1, spOpts);      
+      return res;
     }
 
     public static async FixMissDisForm(spHttpClient:SPHttpClient, siteAbsoluteUrl:string, listTitle:string)
-    {
-      // Get list ID
+    {      
       var apiUrl = `${siteAbsoluteUrl}/_api/web/Lists/getByTitle('${listTitle}')`;
       var res = await spHttpClient.get(apiUrl, SPHttpClient.configurations.v1);
       var resJson = await res.json();
-
-      // 
+      console.log(resJson);       
+      var listId = resJson.Id; // get list ID resJson.Id
+      
+      // get list root folder https://chengc.sharepoint.com/sites/abc/_api/web/Lists/getByTitle('LargeList')/rootFolder/      
+      var rootfolderApi = apiUrl+"/rootFolder"
+      var rootFolderRes = await spHttpClient.get(rootfolderApi, SPHttpClient.configurations.v1);
+      var rootFolderResJson = await rootFolderRes.json();
+      var formPath =  `${rootFolderResJson.ServerRelativeUrl}`;      
+      
+      var webpartId = SPOQAHelper.GenerateUUID();
+      var displayFormHtml = `<%@ Page language="C#" MasterPageFile="~masterurl/default.master"    Inherits="Microsoft.SharePoint.WebPartPages.WebPartPage,Microsoft.SharePoint,Version=16.0.0.0,Culture=neutral,PublicKeyToken=71e9bce111e9429c" meta:webpartpageexpansion="full" meta:progid="SharePoint.WebPartPage.Document"  %>
+      <%@ Register Tagprefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=16.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %> <%@ Register Tagprefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=16.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %> <%@ Import Namespace="Microsoft.SharePoint" %> <%@ Assembly Name="Microsoft.Web.CommandUI, Version=16.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %> <%@ Register Tagprefix="WebPartPages" Namespace="Microsoft.SharePoint.WebPartPages" Assembly="Microsoft.SharePoint, Version=16.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+      <asp:Content ContentPlaceHolderId="PlaceHolderPageTitle" runat="server">
+        <SharePoint:ListFormPageTitle runat="server"/>
+      </asp:Content>
+      <asp:Content ContentPlaceHolderId="PlaceHolderPageTitleInTitleArea" runat="server">
+        <span class="die">
+          <SharePoint:ListProperty Property="LinkTitle" runat="server" id="ID_LinkTitle"/>
+        </span>
+      </asp:Content>
+      <asp:Content ContentPlaceHolderId="PlaceHolderPageImage" runat="server">
+        <img src="/_layouts/15/images/blank.gif?rev=47" width='1' height='1' alt="" data-accessibility-nocheck="true"/>
+      </asp:Content>
+      <asp:Content ContentPlaceHolderId="PlaceHolderMain" runat="server">
+      <SharePoint:UIVersionedContent UIVersion="4" runat="server">
+        <ContentTemplate>
+        <div style="padding-left:5px">
+        </ContentTemplate>
+      </SharePoint:UIVersionedContent>
+        <table class="ms-core-tableNoSpace" id="onetIDListForm" role="presentation">
+         <tr>
+          <td>
+         <WebPartPages:WebPartZone runat="server" FrameType="None" ID="Main" Title="loc:Main"><ZoneTemplate>
+      <WebPartPages:ListFormWebPart runat="server" __MarkupType="xmlmarkup" WebPart="true" __WebPartId="{${webpartId}}" >
+      <WebPart xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://schemas.microsoft.com/WebPart/v2">
+        <Title>${listTitle}</Title>
+        <FrameType>Default</FrameType>
+        <Description />
+        <IsIncluded>true</IsIncluded>
+        <PartOrder>2</PartOrder>
+        <FrameState>Normal</FrameState>
+        <Height />
+        <Width />
+        <AllowRemove>true</AllowRemove>
+        <AllowZoneChange>true</AllowZoneChange>
+        <AllowMinimize>true</AllowMinimize>
+        <AllowConnect>true</AllowConnect>
+        <AllowEdit>true</AllowEdit>
+        <AllowHide>true</AllowHide>
+        <IsVisible>true</IsVisible>
+        <DetailLink />
+        <HelpLink />
+        <HelpMode>Modeless</HelpMode>
+        <Dir>Default</Dir>
+        <PartImageSmall />
+        <MissingAssembly>Cannot import this Web Part.</MissingAssembly>
+        <PartImageLarge />
+        <IsIncludedFilter />
+        <ExportControlledProperties>true</ExportControlledProperties>
+        <ConnectionID>00000000-0000-0000-0000-000000000000</ConnectionID>
+        <ID>g_782eb163_0b9e_4d6c_ba14_89a17fba9c75</ID>
+        <ListName xmlns="http://schemas.microsoft.com/WebPart/v2/ListForm">{${listId}}</ListName>
+        <ListId xmlns="http://schemas.microsoft.com/WebPart/v2/ListForm">${listId}</ListId>
+        <PageType xmlns="http://schemas.microsoft.com/WebPart/v2/ListForm">PAGE_DISPLAYFORM</PageType>
+        <FormType xmlns="http://schemas.microsoft.com/WebPart/v2/ListForm">4</FormType>
+        <ControlMode xmlns="http://schemas.microsoft.com/WebPart/v2/ListForm">Display</ControlMode>
+        <ViewFlag xmlns="http://schemas.microsoft.com/WebPart/v2/ListForm">0</ViewFlag>
+        <ListItemId xmlns="http://schemas.microsoft.com/WebPart/v2/ListForm">0</ListItemId>
+      </WebPart>
+      </WebPartPages:ListFormWebPart>
+          </ZoneTemplate></WebPartPages:WebPartZone>
+          </td>
+         </tr>
+        </table>
+      <SharePoint:UIVersionedContent UIVersion="4" runat="server">
+        <ContentTemplate>
+        </div>
+        </ContentTemplate>
+      </SharePoint:UIVersionedContent>
+      </asp:Content>
+      <asp:Content ContentPlaceHolderId="PlaceHolderAdditionalPageHead" runat="server">
+        <SharePoint:DelegateControl runat="server" ControlId="FormCustomRedirectControl" AllowMultipleControls="true"/>
+        <SharePoint:UIVersionedContent UIVersion="4" runat="server"><ContentTemplate>
+          <SharePoint:CssRegistration Name="forms.css" runat="server"/>
+        </ContentTemplate></SharePoint:UIVersionedContent>
+      </asp:Content>
+      <asp:Content ContentPlaceHolderId="PlaceHolderTitleLeftBorder" runat="server">
+      <table cellpadding="0" height="100%" width="100%" cellspacing="0">
+       <tr><td class="ms-areaseparatorleft"><img src="/_layouts/15/images/blank.gif?rev=47" width='1' height='1' alt="" data-accessibility-nocheck="true"/></td></tr>
+      </table>
+      </asp:Content>
+      <asp:Content ContentPlaceHolderId="PlaceHolderTitleAreaClass" runat="server">
+      <script type="text/javascript" id="onetidPageTitleAreaFrameScript">
+        if (document.getElementById("onetidPageTitleAreaFrame") != null)
+        {
+          document.getElementById("onetidPageTitleAreaFrame").className="ms-areaseparator";
+        }
+      </script>
+      </asp:Content>
+      <asp:Content ContentPlaceHolderId="PlaceHolderBodyAreaClass" runat="server">
+      <SharePoint:StyleBlock runat="server">.ms-bodyareaframe {
+        padding: 8px;
+        border: none;
+      } </SharePoint:StyleBlock>
+      </asp:Content>
+      <asp:Content ContentPlaceHolderId="PlaceHolderBodyLeftBorder" runat="server">
+      <div class='ms-areaseparatorleft'><img src="/_layouts/15/images/blank.gif?rev=47" width='8' height='100%' alt="" data-accessibility-nocheck="true"/></div>
+      </asp:Content>
+      <asp:Content ContentPlaceHolderId="PlaceHolderTitleRightMargin" runat="server">
+      <div class='ms-areaseparatorright'><img src="/_layouts/15/images/blank.gif?rev=47" width='8' height='100%' alt="" data-accessibility-nocheck="true"/></div>
+      </asp:Content>
+      <asp:Content ContentPlaceHolderId="PlaceHolderBodyRightMargin" runat="server">
+      <div class='ms-areaseparatorright'><img src="/_layouts/15/images/blank.gif?rev=47" width='8' height='100%' alt="" data-accessibility-nocheck="true"/></div>
+      </asp:Content>
+      <asp:Content ContentPlaceHolderId="PlaceHolderTitleAreaSeparator" runat="server"/>`;
+      
+       var addFileApiUrl = `${siteAbsoluteUrl}/_api/web/GetFolderByServerRelativeUrl('${formPath}')/Files/Add(url='DispForm.aspx', overwrite=true)`;
+       let spOpts : ISPHttpClientOptions  = {
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        },
+        body: displayFormHtml        
+      };
+      
+      var addFileRes = await spHttpClient.post(addFileApiUrl, SPHttpClient.configurations.v1, spOpts)
+      return addFileRes;
     }
 
     public static async FixDraftVersion(spHttpClient:SPHttpClient, siteAbsoluteUrl:string, isDocument:boolean, listTitle:string, fullDocmentPath:string)
@@ -285,8 +409,9 @@ export default class RestAPIHelper
         
         // /_api/web/getfilebyserverrelativeurl('Server Relative URL%')/CheckIn(comment='Check-in by SharePointOnlineQuickAssist',checkintype=1)
         // "X-HTTP-Method": "PATCH",
+        // https://chengc.sharepoint.com/sites/abc/TestSPOQA/_api/web/GetFileByServerRelativePath(DecodedUrl=@a1)/Publish(@a2)?@a1=%27%2Fsites%2Fabc%2FTestSPOQA%2FShared%20Documents%2FDocument2%2Edocx%27&@a2=%27Looks%20good%27
+        var resJson;
         var relativeDocPath = fullDocmentPath.replace(`https://${document.location.hostname}`, "");
-        let apiUrl:string = `${siteAbsoluteUrl}/_api/web/GetFileByUrl('${relativeDocPath}/CheckIn(comment='Check-in by SharePointOnlineQuickAssist',checkintype=1)`;
         let spOpts = {  
           headers: {              
             "Accept": "application/json;odata=verbose",            
@@ -295,8 +420,19 @@ export default class RestAPIHelper
             "X-HTTP-Method": "PATCH"            
           }          
         };  
-      var res = await spHttpClient.post(apiUrl, SPHttpClient.configurations.v1, spOpts); 
-      var resJson = await res.json();
+                
+          let apiUrl:string = `${siteAbsoluteUrl}/_api/web/GetFileByUrl('${relativeDocPath}')/CheckIn(comment='Check-in by SharePointOnlineQuickAssist',checkintype=1)`;           
+          var res = await spHttpClient.post(apiUrl, SPHttpClient.configurations.v1, spOpts); 
+          resJson = await res.json();
+          console.log(resJson);
+          if(resJson.error)
+          {
+            let apiUrl:string = `${siteAbsoluteUrl}/_api/web/GetFileByUrl('${relativeDocPath}')/Publish('Published by SharePointOnlineQuickAssist')`;           
+            var res = await spHttpClient.post(apiUrl, SPHttpClient.configurations.v1, spOpts); 
+            resJson = await res.json();
+          }
+     
+
       return resJson;
     }
 }

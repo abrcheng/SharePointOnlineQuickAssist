c5cdb7a5c76f4e6043c1ec3b539eacd1b6a913db
Abraham Cheng <abrcheng@microsoft.com>
2021-11-27 09:40:38 +0800

    Added UpdateTermOwner.ps1
    Added UpdateTermOwner.ps1

---
+++ KBs/TermStore/UpdateTermOwner/UpdateTermOwner.ps1
@@ -0,0 +1,37 @@
+﻿Connect-PnPOnline https://chengc.sharepoint.com -UseWebLogin
+$TermGroupName = "Site Collection - chengc.sharepoint.com"
+$TermSetName= "SiteMM"
+$TermSetOwner= "i:0#.f|membership|johna@chengc.onmicrosoft.com"
+$processedCount = 0
+
+$Ctx = Get-PnPContext
+$TaxonomySession = [Microsoft.SharePoint.Client.Taxonomy.TaxonomySession]::GetTaxonomySession($Ctx)
+$TaxonomySession.UpdateCache()
+$Ctx.Load($TaxonomySession)
+
+$TermStore = $TaxonomySession.GetDefaultSiteCollectionTermStore()
+$Ctx.Load($TermStore)
+$Ctx.ExecuteQuery()
+ 
+$TermGroup = $TermStore.Groups.GetByName($TermGroupName)
+$Ctx.Load($TermGroup)
+
+#Get the termset
+$TermSet = $TermGroup.TermSets.GetByName($TermSetName)
+$Ctx.Load($TermSet)
+$Ctx.ExecuteQuery()
+
+$terms = $TermSet.GetAllTerms()
+$Ctx.Load($terms)
+$Ctx.ExecuteQuery()
+
+foreach($term in $terms)
+{
+  Write-Host "Processing $($term.Name) ....."
+  $term.Owner = $TermSetOwner
+  $processedCount++
+}
+
+$TermStore.CommitAll()
+$Ctx.ExecuteQuery()
+Write-host "Processed $processedCount terms, Done" -ForegroundColor Green

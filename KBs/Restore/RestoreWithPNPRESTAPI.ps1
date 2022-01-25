169113fa7edce8c75ab1db4eaf367a6e166800fd
Abraham Cheng <abrcheng@microsoft.com>
2022-01-25 15:29:55 +0800

    Added GetUsersWithOutPictureInSPO.ps1
    Added GetUsersWithOutPictureInSPO.ps1

---
+++ KBs/Restore/RestoreWithPNPRESTAPI.ps1
@@ -0,0 +1,37 @@
+﻿$StartDate = "2019/01/24 9:00:00"
+$EndDate = "2019/01/25 9:00:00"
+$SiteURL = "https://chengc.sharepoint.com"
+
+Connect-PnPOnline $SiteURL  
+
+$DeletedCollection = Get-PnPRecyclebinItem -RowLimit 500 | ? { ($_.DeletedByEmail -eq 'UserEmailID') -and ($_.DeletedDate -ge $StartDate) -and ($_.DeletedDate -le $EndDate)}
+                                                                              
+ $ctx = Get-PnPContext
+ 
+
+foreach($DeletedItem in $DeletedCollection)
+{
+    Try
+    {
+    Write-host "Initiating Restore for: "  $DeletedItem.Title
+$RestoreURL = $SiteURL+"/_api/web/Recyclebin('$($DeletedItem.ID)')/restore()"
+    Invoke-PnPSPRestMethod -Method post -Url $RestoreURL 
+
+    Write-host "Restore Completed for: "  $DeletedItem.Title
+    Write-Host -ForegroundColor Green "Success :: File is successfully restored"
+    Write-host "Writing to log file.................____________________________________________ " $Successcount
+    $Successcount++
+    }
+    Catch
+    {
+     Write-host "Exception for: "  $DeletedItem.Title
+     $errormessage = "Catch Error:" + $_.Exception.Message
+     Write-host "Writing to log file.................____________________________________________ " $FailureCount
+    $FailureCount++
+    Write-Host -ForegroundColor Red "Catch :: There is an error while restoring the Document"  $_.Exception.Message
+    Continue
+    }
+}
+
+Write-Host -ForegroundColor DarkGreen "Success Count: " + $Successcount
+Write-Host -ForegroundColor DarkRed "Failure Count: " + $FailureCount    
---
+++ KBs/UAP/SyncPhotoFromADToSPO/GetUsersWithOutPictureInSPO.ps1
@@ -0,0 +1,34 @@
+﻿param( 
+  [string]$adminSiteUrl = "https://chengc-admin.sharepoint.com"  
+)
+
+Connect-PnPOnline -Url $adminSiteUrl -UseWebLogin
+$clientContext = Get-PnPContext
+Connect-AzureAD
+$allUsers = Get-AzureADUser -all $true
+$userCount =0
+foreach($user in $allUsers)
+{
+    if($user.UserPrincipalName.Contains("#EXT#") -ne $true)
+    {
+       Write-Host "Processing $($user.UserPrincipalName)"
+       try
+       {
+           $Properties = Get-PnPUserProfileProperty -Account $user.UserPrincipalName
+           if([System.String]::IsNullOrWhiteSpace($Properties.PictureUrl))
+           {
+                 Write-Host  $user.UserPrincipalName
+                 $user.UserPrincipalName >>"UsersWithOutPictureInSPO.txt"
+                 $userCount++
+           }
+       }
+       catch [System.Exception] 
+       {
+          $errormessage = $_.Exception.ToString()
+          "Get error message $errormessage when processing $($user.UserPrincipalName) \r\n" >>"ErrorMessage.txt"  
+       }       
+    }
+}
+ 
+Write-Host "Detected $userCount users without picture in the SPO user profile, please check UsersWithOutPictureInSPO.txt for detail"
+Write-Host "Done" -ForegroundColor Green

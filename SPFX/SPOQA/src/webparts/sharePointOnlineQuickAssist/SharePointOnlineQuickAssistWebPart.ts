201460f8aa5e9a1ee6a32bd65598134562e29056
Abraham Cheng <abrcheng@microsoft.com>
2022-07-06 16:32:45 +0800

    Fixed validation issues
    Fixed validation issues

--- SPFX/SPOQA/src/webparts/sharePointOnlineQuickAssist/SharePointOnlineQuickAssistWebPart.ts
+++ SPFX/SPOQA/src/webparts/sharePointOnlineQuickAssist/SharePointOnlineQuickAssistWebPart.ts
@@ -65,23 +65,7 @@ export default class SharePointOnlineQuickAssistWebPart extends BaseClientSideWe
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
     return {
-      pages: [
-        {
-          header: {
-            description: strings.PropertyPaneDescription
-          },
-          groups: [
-            {
-              groupName: strings.BasicGroupName,
-              groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
-                })
-              ]
-            }
-          ]
-        }
-      ]
+      pages: []
     };
   }
 }
